e8d5c838bc25e26b161b5d017914ed84aec26283
Szymon Gornioczek <szymon.gornioczek@agora.pl>
2020-03-15 22:54:49 +0100

    Falling box

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+// create an engine
+var engine;
+var box1;
+var world;
+
+
 function setup() {
   createCanvas(400, 400);
+  engine = Engine.create();
+  world = engine.world;
+  box1 = Bodies.rectangle(200, 100, 80, 80);
+  Engine.run(engine);
+  World.add(world, box1);
 }
 
 function draw() {
-  background(220);
+  background(40);
+  rect(box1.position.x, box1.position.y, 80,80);
 }
